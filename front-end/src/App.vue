59d62d87d669adb6a9b6eb460107ed71fb7a17e5
ls <git>
2019-06-12 14:12:58 +0800

    4. Vue.js调用API实现用户注册/登录/退出

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,23 +1,27 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <navbar></navbar>
     <router-view/>
   </div>
 </template>
 
 <script>
+import Navbar from './components/Navbar'
+import Home from './components/Home'
+import Login from './components/Login'
+import Register from './components/Register'
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    navbar: Navbar,
+    home: Home,
+    login: Login,
+    register: Register,
+  }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
